c8d97d5dee7028ff34df8a25220e221e1ceeed37
Daniel Kohli <drkohlipk@yahoo.com>
2016-07-03 16:22:40 -0700

    Add files via upload

---
+++ resume/js/resume.js
@@ -0,0 +1,5 @@
+/*var email = */document.getElementById("e-mail").onclick="location.href='http://www.stackoverflow.com'";
+/*var clickEmail = function() {
+    window.url = "http://www.stackoverflow.com";
+};
+email.AddEventListener("click", clickEmail);*/
