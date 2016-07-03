110f92752dbff117a4f502664687911e758ea51b
Daniel Kohli <drkohlipk@yahoo.com>
2016-07-03 16:23:59 -0700

    Add files via upload

--- resume/js/resume.js
+++ resume/js/resume.js
@@ -1,15 +1,5 @@
-$(document).ready(function() {
-    $(window).resize(function() {
-        $(".left-topimg > img").each(function() {
-            var cHeight = $(this).parent(".left-topimg").height(),
-                cWidth = $(this).parent(".left-topimg").width(),
-                iHeight = $(this).height(),
-                iWidth = $(this).width();
-
-            $(this).css({
-                top: 0.5*(cHeight - iHeight),
-                left: 0.5*(cWidth - iWidth)
-            });
-        });
-    }).resize();  // Fires resize event when document is first loaded
-});
+/*var email = */document.getElementById("e-mail").onclick="location.href='http://www.stackoverflow.com'";
+/*var clickEmail = function() {
+    window.url = "http://www.stackoverflow.com";
+};
+email.AddEventListener("click", clickEmail);*/
