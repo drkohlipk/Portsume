d97fc502c781e4e0b36da227b9d5b244f0d2c0d4
DANIEL KOHLI <drkohlipk@yahoo.com>
2016-07-06 23:28:23 -0700

    Update to folder and Header

--- resume/js/resume.js
+++
@@ -1,5 +0,0 @@
-/*var email = */document.getElementById("e-mail").onclick="location.href='http://www.stackoverflow.com'";
-/*var clickEmail = function() {
-    window.url = "http://www.stackoverflow.com";
-};
-email.AddEventListener("click", clickEmail);*/
